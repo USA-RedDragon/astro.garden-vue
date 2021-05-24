7d399049d318436725dbadb44617e30fde4b546c
Jacob McSwain <jacob.a.mcswain@gmail.com>
2021-05-24 00:29:54 -0500

    Add gallery

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <h1>Welcome to Astro Garden</h1>
+    <div id="nav">
+      <router-link to="/">Gallery</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
