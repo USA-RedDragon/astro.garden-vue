3b92284d8b662b204fc175c3a0688ad7da99f5aa
Jacob McSwain <jacob.a.mcswain@gmail.com>
2021-05-24 03:04:46 -0500

    Add more photos to gallery

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -3,45 +3,63 @@
     <h1>Gallery</h1>
     <h2>Datasource: Myself</h2>
     <section class="photos">
-      <div>
-        <p><img src="../assets/my-data/milky_way.png" /><br />The Milky Way</p>
-      </div>
-      <div>
-        <p><img src="../assets/my-data/moon.png" /><br />The Moon</p>
-      </div>
+      <Photo
+        :src="require('@/assets/my-data/milky_way.png')"
+        text="The Milky Way"
+      />
+      <Photo :src="require('@/assets/my-data/moon.png')" text="The Moon" />
     </section>
     <h2>Datasource: <a href="https://telescope.live">Telescope.live</a></h2>
     <section class="photos">
-      <div>
-        <p>
-          <img src="../assets/other-data/Centaurus_a.png" /><br />Centaurus A
-        </p>
-      </div>
-      <div>
-        <p><img src="../assets/other-data/rim-nebula.png" /><br />Rim Nebula</p>
-      </div>
-      <div>
-        <p>
-          <img src="../assets/other-data/Rosette.png" /><br />Rosette Nebula
-        </p>
-      </div>
-      <div>
-        <p>
-          <img src="../assets/other-data/tarantula.png" /><br />Tarantula Nebula
-        </p>
-      </div>
-      <div>
-        <p><img src="../assets/other-data/veil.png" /><br />Veil Nebula</p>
-      </div>
-      <div>
-        <p>
-          <img src="../assets/other-data/vela.png" /><br />Vela Supernova
-          Remnant
-        </p>
-      </div>
+      <Photo
+        :src="require('@/assets/other-data/Centaurus_a.png')"
+        text="Centaurus A"
+      />
+      <Photo
+        :src="require('@/assets/other-data/rim-nebula.png')"
+        text="Rim Nebula"
+      />
+      <Photo
+        :src="require('@/assets/other-data/Rosette.png')"
+        text="Rosette Nebula"
+      />
+      <Photo
+        :src="require('@/assets/other-data/tarantula.png')"
+        text="Tarantula Nebula"
+      />
+      <Photo
+        :src="require('@/assets/other-data/veil.png')"
+        text="Veil Nebula"
+      />
+      <Photo
+        :src="require('@/assets/other-data/vela.png')"
+        text="Vela Supernova Remnant"
+      />
+      <Photo
+        :src="require('@/assets/other-data/carina-nebula.png')"
+        text="Carina Nebula"
+      />
+      <Photo :src="require('@/assets/other-data/m22.png')" text="M22" />
+      <Photo
+        :src="require('@/assets/other-data/statue-of-liberty-nebula.png')"
+        text="Statue of Liberty Nebula"
+      />
+      <Photo
+        :src="require('@/assets/other-data/eagle-nebula.png')"
+        text="Eagle Nebula"
+      />
     </section>
   </div>
 </template>
+
+<script>
+import Photo from '../components/Photo';
+export default {
+  components: {
+    Photo,
+  },
+};
+</script>
 
 <style scoped>
 .photos {
