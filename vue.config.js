7bb156ce08a7b4ca8885340002b68a43c6416999
Jacob McSwain <jacob.a.mcswain@gmail.com>
2021-06-18 02:04:26 -0500

    Vue: Use contenthash

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   lintOnSave: false,
+  chainWebpack: (config) => {
+    if (config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css');
+      extractCSSPlugin && extractCSSPlugin.tap(() => [{
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].css',
+      }]);
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+    },
+  },
 };
