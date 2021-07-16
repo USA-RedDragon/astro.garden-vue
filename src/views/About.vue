6eea66eeea6fbdf1172101477ec92369bc75d2b3
Jacob McSwain <jacob.a.mcswain@gmail.com>
2021-07-15 23:04:05 -0500

    Fixes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Me</h1>
+
+    <picture class="rounded img-responsive" style="width: 100%">
+      <source :srcset="require('@/assets/me.webp')" type="image/webp" />
+      <source :srcset="require('@/assets/me.png')" type="image/png" />
+      <img
+        style="width: 20%; border-radius: 16rem"
+        src="assets/me.png"
+        alt="Me, Jacob McSwain"
+      />
+    </picture>
+
+    <p>
+      Astro Garden is my personal gallery website for showcasing my
+      astrophotography work. I started astrophotography in May of 2021 with a
+      Nikon D5500 and a tripod. Now, I've got a dedicated astrophotography rig
+      and soon some space for a remote observatory in western Oklahoma. This is
+      the only place you can view my photos at full resolution for free.
+    </p>
+
+    <h2>My Astrophotography Rig</h2>
+    <img :src="require('@/assets/about/gear.jpg')" />
   </div>
 </template>
