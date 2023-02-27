a89a0c8b89ff27499a815c174b0c3859b3d127c4
Jacob McSwain <jacob@mcswain.dev>
2023-02-26 22:42:02 -0600

    Fix lint issues

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   configureWebpack: {
     output: {
-      filename: '[name].[contenthash].js',
+      filename: '[name].[hash].js',
       chunkFilename: '[name].[contenthash].js',
     },
   },
